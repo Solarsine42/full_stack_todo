359c65387115b30996925653dc1b87a911815d44
Solarsine42 <Solarsine42@gmail.com>
2019-10-07 18:38:12 -0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React {
+  store = {
+    todos: [],
+    message: ""
+  };
+
+  render() {
+    return <div className="App">FS Todo App</div>;
+  }
 }
 
 export default App;
